05e8295f9064edb50304d71b9fc99318ba956c21
Lucas Bernadino <luucasdinoo@gmail.com>
2024-06-18 16:46:16 -0300

    Aprendendo sobre content negotiation

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/controllers/PersonController.java
@@ -3,35 +3,37 @@ package br.com.erudio.restwithspringbootandjavaerudio.controllers;
 import br.com.erudio.restwithspringbootandjavaerudio.model.dto.PersonDto;
 import br.com.erudio.restwithspringbootandjavaerudio.services.PersonService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import br.com.erudio.restwithspringbootandjavaerudio.util.MediaType;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 @RequiredArgsConstructor
 public class PersonController {
 
     private final PersonService personService;
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public PersonDto findById(@PathVariable(value = "id") Long id) {
         return personService.findById(id);
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public List<PersonDto> findAll() {
         return personService.findAll();
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE ,produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public PersonDto create(@RequestBody PersonDto person) {
         return personService.create(person);
     }
 
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE ,produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public PersonDto update(@RequestBody PersonDto person) {
         return personService.update(person);
     }
