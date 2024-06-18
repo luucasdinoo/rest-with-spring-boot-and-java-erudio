0144d14f4fb524cd3ea7655c143ccfe9a618ea0b
Lucas Bernadino <luucasdinoo@gmail.com>
2024-06-18 17:08:30 -0300

    Implementando Hateoas

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/model/dto/PersonDto.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/model/dto/PersonDto.java
@@ -1,5 +1,6 @@
 package br.com.erudio.restwithspringbootandjavaerudio.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import java.io.Serializable;
 @JsonPropertyOrder({"id","firstName","lastName", "address", "gender"})
 public class PersonDto extends RepresentationModel<PersonDto> implements Serializable {
 
+    @JsonProperty("id")
     private Long key;
     private String firstName;
     private String lastName;
--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/services/PersonService.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/services/PersonService.java
@@ -1,5 +1,6 @@
 package br.com.erudio.restwithspringbootandjavaerudio.services;
 
+import br.com.erudio.restwithspringbootandjavaerudio.controllers.PersonController;
 import br.com.erudio.restwithspringbootandjavaerudio.mapper.ModelMapper;
 import br.com.erudio.restwithspringbootandjavaerudio.model.dto.PersonDto;
 import br.com.erudio.restwithspringbootandjavaerudio.exceptions.ResourceNotFoundException;
@@ -8,6 +9,9 @@ import br.com.erudio.restwithspringbootandjavaerudio.repositories.PersonReposito
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 
 import java.util.List;
 
@@ -21,18 +25,26 @@ public class PersonService {
     public PersonDto findById(Long id){
         log.info("Finding one person!");
         Person entity = personRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
-        return ModelMapper.parseObject(entity, PersonDto.class);
+        PersonDto dto = ModelMapper.parseObject(entity, PersonDto.class);
+        // HATEOES
+        dto.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+        return dto;
     }
 
     public List<PersonDto> findAll(){
         log.info("Finding all persons!");
-        return ModelMapper.parseListObjects(personRepository.findAll(),PersonDto.class);
+        List<PersonDto> persons = ModelMapper.parseListObjects(personRepository.findAll(),PersonDto.class);
+        persons.stream()
+                .forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+        return persons;
     }
 
     public PersonDto create(PersonDto person){
         log.info("Creating one person!");
         Person entity = ModelMapper.parseObject(person, Person.class);
-        return ModelMapper.parseObject(personRepository.save(entity), PersonDto.class);
+        PersonDto dto = ModelMapper.parseObject(personRepository.save(entity), PersonDto.class);
+        dto.add(linkTo(methodOn(PersonController.class).findById(dto.getKey())).withSelfRel());
+        return dto;
     }
 
     public PersonDto update(PersonDto person){
@@ -42,7 +54,9 @@ public class PersonService {
         entity.setLastName(person.getLastName());
         entity.setAddress(person.getAddress());
         entity.setGender(person.getGender());
-        return ModelMapper.parseObject(personRepository.save(entity), PersonDto.class);
+        PersonDto dto = ModelMapper.parseObject(personRepository.save(entity), PersonDto.class);
+        dto.add(linkTo(methodOn(PersonController.class).findById(dto.getKey())).withSelfRel());
+        return dto;
     }
 
     public void delete(Long id){
