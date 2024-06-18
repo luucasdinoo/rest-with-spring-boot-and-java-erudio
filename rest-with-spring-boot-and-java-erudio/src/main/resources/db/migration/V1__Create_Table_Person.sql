8b9a0b4283e4da17137f8590731aad64ffb582ed
Lucas Bernadino <luucasdinoo@gmail.com>
2024-06-18 16:45:19 -0300

    Aprendendo sobre migrations

---
+++ rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,9 @@
+create table person
+(
+    id         bigint auto_increment primary key,
+    first_name varchar(80)  not null,
+    last_name  varchar(80)  not null,
+    address    varchar(100) not null,
+    gender     varchar(6)   not null
+);
+
---
+++ rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO rest_with_spring_boot_erudio_db.person (id, address, first_name, gender, last_name) VALUES (1, 'Recife-Pernambuco-BR', 'Lucas', 'Male', 'Bernadino');
+INSERT INTO rest_with_spring_boot_erudio_db.person (id, address, first_name, gender, last_name) VALUES (2, 'São Paulo ', 'Aryton', 'Male', 'Sena');
+INSERT INTO rest_with_spring_boot_erudio_db.person (id, address, first_name, gender, last_name) VALUES (4, 'Kentucky US', 'Muhamed', 'Male', 'Ali');
+INSERT INTO rest_with_spring_boot_erudio_db.person (id, address, first_name, gender, last_name) VALUES (5, 'Jaboatão PE', 'Lucas', 'Male', 'Bomfim');
